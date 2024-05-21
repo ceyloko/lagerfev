e3afb6b039d4c8bc0bf5fd67058a801bcb7b58cd
ceyloko <65474465+ceyloko@users.noreply.github.com>
2024-05-21 15:05:13 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ import os
 class Config():
     # Get these values from my.telegram.org
     # https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_NAME = os.environ.get("BOT_NAME")
-    BOT_ID = int(os.environ.get("BOT_ID"))
-    SUDO_USERS = os.environ.get("SUDO_USERS").split()
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT")
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    API_ID = int(os.environ.get("27182917"))
+    API_HASH = os.environ.get("e627c805adc5719f484d84fc829bc11b")
+    BOT_TOKEN = os.environ.get("6817520332:AAFW4PCppQlrjaLo1Rhsic4Vz2lNwxsFpH4")
+    BOT_USERNAME = os.environ.get("feverantaggerbot")
+    BOT_NAME = os.environ.get("FeveranTaggerBot")
+    BOT_ID = int(os.environ.get("6817520332"))
+    SUDO_USERS = os.environ.get("1111099757").split()
+    SUPPORT_CHAT = os.environ.get("https://t.me/feveranchat")
+    OWNER_ID = int(os.environ.get("1111099757"))
+    OWNER_USERNAME = os.environ.get("yusufuslu")
